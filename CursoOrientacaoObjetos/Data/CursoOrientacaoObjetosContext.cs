47c9b07e48acd6d5ac29ef9c4925eec0524ac96d
AriSilva94 <ariovaldo.bsjunior@gmail.com>
2021-02-17 20:33:52 -0300

    CRUD Scaffolding

---
+++ CursoOrientacaoObjetos/Data/CursoOrientacaoObjetosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CursoOrientacaoObjetos.Models;
+
+namespace CursoOrientacaoObjetos.Data
+{
+    public class CursoOrientacaoObjetosContext : DbContext
+    {
+        public CursoOrientacaoObjetosContext (DbContextOptions<CursoOrientacaoObjetosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CursoOrientacaoObjetos.Models.Department> Department { get; set; }
+    }
+}
--- CursoOrientacaoObjetos/Startup.cs
+++ CursoOrientacaoObjetos/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CursoOrientacaoObjetos.Data;
 
 namespace CursoOrientacaoObjetos
 {
@@ -33,6 +35,9 @@ namespace CursoOrientacaoObjetos
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CursoOrientacaoObjetosContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CursoOrientacaoObjetosContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
