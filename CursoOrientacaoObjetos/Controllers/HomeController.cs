83815986021250933be6bdcf7cd9867455753842
AriSilva94 <ariovaldo.bsjunior@gmail.com>
2021-02-17 20:28:02 -0300

    First Model-Controller-View - Departmenet

--- CursoOrientacaoObjetos/Controllers/HomeController.cs
+++ CursoOrientacaoObjetos/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CursoOrientacaoObjetos.Models;
 using Microsoft.AspNetCore.Mvc;
-using CursoOrientacaoObjetos.Models;
+using System.Diagnostics;
 
 namespace CursoOrientacaoObjetos.Controllers
 {
