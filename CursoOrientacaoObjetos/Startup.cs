3903a5fc12d375d991adcc04b48daf80899a093d
Ariovaldo <ariovaldo.bsjunior@gmail.com>
2021-02-17 22:13:52 -0300

    MySQL adaptation and first migration

--- CursoOrientacaoObjetos/Startup.cs
+++ CursoOrientacaoObjetos/Startup.cs
@@ -37,7 +37,8 @@ namespace CursoOrientacaoObjetos
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<CursoOrientacaoObjetosContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CursoOrientacaoObjetosContext")));
+                    options.UseMySql(Configuration.GetConnectionString("CursoOrientacaoObjetosContext"), builder =>
+                    builder.MigrationsAssembly("CursoOrientacaoObjetos")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
