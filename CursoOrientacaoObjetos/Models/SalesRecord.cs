da72c3ca1f9c1284edbc2166d59f50342d9fea2d
Ariovaldo <ariovaldo.bsjunior@gmail.com>
2021-02-21 06:10:27 -0300

    Implementing grouping search

--- CursoOrientacaoObjetos/Models/SalesRecord.cs
+++ CursoOrientacaoObjetos/Models/SalesRecord.cs
@@ -1,12 +1,17 @@
 ﻿using CursoOrientacaoObjetos.Models.Enum;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CursoOrientacaoObjetos.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
 
