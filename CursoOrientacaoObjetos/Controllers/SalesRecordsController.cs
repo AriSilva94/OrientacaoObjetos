4480d48bf3959ca5e5ec019acb29121d32ddc594
Ariovaldo <ariovaldo.bsjunior@gmail.com>
2021-02-21 05:28:15 -0300

    Preparing sales search navigation views

---
+++ CursoOrientacaoObjetos/Controllers/SalesRecordsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CursoOrientacaoObjetos.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
