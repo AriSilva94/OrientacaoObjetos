26187a88a025fecd0f72345ca6ad75f41251d2ce
Ariovaldo <ariovaldo.bsjunior@gmail.com>
2021-02-21 05:53:33 -0300

    Implementing simple search

---
+++ CursoOrientacaoObjetos/Services/SalesRecordService.cs
@@ -0,0 +1,38 @@
+﻿using CursoOrientacaoObjetos.Data;
+using CursoOrientacaoObjetos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoOrientacaoObjetos.Services
+{
+    public class SalesRecordService
+    {
+        private readonly CursoOrientacaoObjetosContext _context;
+
+        public SalesRecordService(CursoOrientacaoObjetosContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
